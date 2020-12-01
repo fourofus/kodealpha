198311517b2b18834327eba3c287a5b2d1533d22
HJ Kim <kode.hjkim@gmail.com>
2020-12-01 23:07:17 +0000

    KA-5: Redirect root URL to greeting default.

--- kodealpha/__init__.py
+++ kodealpha/__init__.py
@@ -20,19 +20,31 @@ class ApiLogger(AbstractAccessLogger):
 class KodeAppFactory:
     def create_app(self):
         app = web.Application(middlewares=[
-            normalize_path_middleware(append_slash=False, remove_slash=True, merge_slashes=True)]
-        )
+            normalize_path_middleware(append_slash=False, remove_slash=True, merge_slashes=True)
+        ])
 
         app.add_routes([
-            web.get('/', self.handler),
-            web.get('/api/{general}', self.handler)
+            web.get('/', self.default_handler),
+            web.get('/{misc}', self.default_handler),
+            web.get('/api/greeting', self.greeting_handler, name='greeting_default'),
+            web.get('/api/greeting/{message}', self.greeting_handler, name='greeting_message'),
         ])
         return app
 
     @staticmethod
-    async def handler(request):
-        general = request.match_info.get('general', 'unknown')
-        return web.Response(text=f'Received "{general}"')
+    async def greeting_handler(request):
+        message = request.match_info.get('message', '')
+        if message:
+            return web.Response(text=f'Received "{message}"')
+        return web.Response(text=f'Hello there, hope you are having a fine day!')
+
+    @staticmethod
+    async def default_handler(request):
+        try:
+            location = request.app.router['greeting_default'].url_for()
+            raise web.HTTPFound(location=location)
+        except KeyError as e:
+            return web.Response(text=f'Cannot redirect. Sorry I failed. "{e.__class__}:{e}"')
 
 
 class KodeService:
